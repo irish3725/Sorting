3ba525ef1c83a340ab3c5f21f3293d9815b0afaf
Alex Irish <irish3725@gmail.com>
2017-01-04 14:48:39 -0700

    fixed issue with interface and added options

--- sort.py
+++ sort.py
@@ -3,15 +3,33 @@
 from random import randint
 
 done = False
+
 while not done:
-	input = input('How large would you like your list?\n> ')
-	length = int(input)
-	print('You inputted ' + input)
-	print('here is your new list:')
+	choiceDone = False
+	instring = input('How large would you like your list?\n> ')
+	length = int(instring)
+	print('You inputted ' + instring)
+	print('Here is your new list:')
 	list = []
 	for i in range(length):
 		list.append(randint(0,100))
 	print("-----------------------------")
 	for i in range(length):
 		print(list[i], end=' ')
-	print("\n-----------------------------")
+	print("\n-----------------------------\n")
+	
+	while not choiceDone:
+		print("Which sort method would you like to use?")
+		instring = input("(1)BubbleSort (2)CoctailShakerSort (3)Quit\n> ")
+		if instring is "1":
+			print("You chose BubbleSort")
+			choiceDone = True
+		if instring is "2":
+			print("you chose CoctailShakerSort")
+			choiceDone = True
+		if instring is "3":
+			choiceDone = True
+			done = True
+		#else 
+		#	print("type either 1 or 2")
+
