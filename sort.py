4a30659b51cd83cb63c2fa5881b42a1565a78a5c
Alex Irish <irish3725@gmail.com>
2017-01-04 00:36:02 -0700

    added loop containing user interface, but I am running into an error with reading input a second time.

--- sort.py
+++ sort.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
+from random import randint
+
 done = False
 while not done:
-	x = input('How large would you like your array?\n> ')
-	print('You inputted ' + x)
-	
-
+	input = input('How large would you like your list?\n> ')
+	length = int(input)
+	print('You inputted ' + input)
+	print('here is your new list:')
+	list = []
+	for i in range(length):
+		list.append(randint(0,100))
+	print("-----------------------------")
+	for i in range(length):
+		print(list[i], end=' ')
+	print("\n-----------------------------")
