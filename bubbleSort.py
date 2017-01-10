d69f824e1ea15e7955b0882c8322fb4280a0856f
Alex Irish <irish3725@gmail.com>
2017-01-10 11:52:06 -0700

    fixed interface, bubble sort now works with interface, still can only run once before breaking.

--- bubbleSort.py
+++ bubbleSort.py
@@ -2,27 +2,32 @@
 
 from random import randint
 
-mylist = []
-for x in range(19):
-    mylist.append(randint(0,100))
+myList = []
+length = 0;
+#for i in range(0, n)
+#	myList.append(randint(0,100))
 
 class C:
+	def generateList(self, args):
+		length = args
+		for i in range(0, length):
+			myList.append(randint(0,100))
 	def printList(self):
 		print("--------------------------------------------------------------")
-		for x in range(19):
-    			print(mylist[x], end=", ")
+		for x in myList:
+ 			print(x, end=" ")
 		print("\n--------------------------------------------------------------")
 
-	def bubbleSort(self):
-		for x in range(19):
-			for y in range(19 - x):
-				if(y < 18 and mylist[y] > mylist[y + 1]):
-					temp = mylist[y + 1]
-					mylist[y + 1] = mylist[y]
-					mylist[y] = temp
-
-
-c = C()
-c.printList()
-c.bubbleSort()
-c.printList()
+	def bubbleSort(self, n):
+		print(n)
+		for x in myList:
+			n = n - 1
+			i = 0
+			for y in myList:
+				if(i < n):
+					if (myList[i] > myList[i + 1]):
+						temp = myList[i + 1]
+						myList[i + 1] = myList[i]
+						myList[i] = temp
+				i = i + 1
+		return myList
--- sort.py
+++ sort.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python3
 
 from random import randint
+from bubbleSort import *
 
 done = False
 
 while not done:
+	c = C()
 	choiceDone = False
 	instring = input('How large would you like your list?\n> ')
 	length = int(instring)
 	print('You inputted ' + instring)
 	print('Here is your new list:')
-	list = []
-	for i in range(length):
-		list.append(randint(0,100))
-	print("-----------------------------")
-	for i in range(length):
-		print(list[i], end=' ')
-	print("\n-----------------------------\n")
+	c.generateList(length)
+	c.printList()
+#	myList = []
+#	for i in range(length):
+#		myList.append(randint(0,100))
+#	c.printList(myList)
+#	print("-----------------------------")
+#	for i in range(length):
+#		print(myList[i], end=' ')
+#	print("\n-----------------------------\n")
 	
 	while not choiceDone:
 		print("Which sort method would you like to use?")
 		instring = input("(1)BubbleSort (2)CoctailShakerSort (3)Quit\n> ")
 		if instring is "1":
 			print("You chose BubbleSort")
+			c.bubbleSort(length)
+			c.printList()
 			choiceDone = True
 		if instring is "2":
 			print("you chose CoctailShakerSort")
@@ -30,6 +37,6 @@ while not done:
 		if instring is "3":
 			choiceDone = True
 			done = True
-		#else 
-		#	print("type either 1 or 2")
+#		else 
+#			print("type either 1 or 2")
 
