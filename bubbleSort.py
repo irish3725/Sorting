0ff7843935bc3dc287f870bbd3f873a1421930f4
Alex Irish <irish3725@gmail.com>
2017-01-10 13:52:34 -0700

    fixed issue with running multiple times and added insertion sort.

--- bubbleSort.py
+++ bubbleSort.py
@@ -4,22 +4,20 @@ from random import randint
 
 myList = []
 length = 0;
-#for i in range(0, n)
-#	myList.append(randint(0,100))
 
 class C:
-	def generateList(self, args):
-		length = args
+	def generateList(self, n, size):
+		global myList
+		myList = []
+		length = n
 		for i in range(0, length):
-			myList.append(randint(0,100))
+			myList.append(randint(0, size))
 	def printList(self):
 		print("--------------------------------------------------------------")
-		for x in myList:
- 			print(x, end=" ")
-		print("\n--------------------------------------------------------------")
+		print(myList)
+		print("--------------------------------------------------------------")
 
 	def bubbleSort(self, n):
-		print(n)
 		for x in myList:
 			n = n - 1
 			i = 0
@@ -31,3 +29,70 @@ class C:
 						myList[i] = temp
 				i = i + 1
 		return myList
+
+	def insertionSort(self):
+		global myList
+		fList = []
+		for i in myList:
+			inserted = False
+			if not fList:
+				fList.append(i)
+			else:
+				index  = -1
+				for j in fList:
+					index = index + 1
+					if(i < j):
+						fList.insert(index, i)
+						inserted = True
+						break
+				if not inserted:
+					fList.append(i)
+		myList = fList
+
+	def freq(self):
+		count = 0
+		minval = 0
+		mincount = -1
+		maxval = 0
+		maxcount = -1
+		val = 0
+		tempcount = 0
+		for i in myList:
+			if(i == val):
+				tempcount = tempcount + 1
+			elif(mincount == -1):
+				maxcount = tempcount
+				maxval = val
+				mincount = tempcount
+				minval = val
+				tempcount = 1
+				val = i
+				count = count + 1
+			elif(tempcount > maxcount):
+				maxcount = tempcount
+				maxval = val
+				tempcount = 1
+				val = i
+				count = count + 1
+			elif(tempcount < mincount or mincount == -1):
+				mincount = tempcount
+				minval = val
+				tempcount = 1
+				val = i
+				count = count + 1
+			else:
+				tempcount = 1
+				val = i
+				count = count + 1
+			if(count < val):
+				mincount = 0
+				minval = count
+				count = val
+		if(tempcount > maxcount):
+			maxcount = tempcount
+			maxval = val
+		if(tempcount < mincount):
+			mincount = tempcount
+			minval = val
+		print("The value that showed up the least was " + str(minval) + ", and it showed up " + str(mincount) + " times.")
+		print("The value that showed up the most was " + str(maxval) + ", and it showed up " + str(maxcount) + " times.")
--- sort.py
+++ sort.py
@@ -11,32 +11,30 @@ while not done:
 	instring = input('How large would you like your list?\n> ')
 	length = int(instring)
 	print('You inputted ' + instring)
+	instring = input('How large would you like the numbers to get?\n> ')
+	size = int(instring)
+	print('You inputted ' + instring)
 	print('Here is your new list:')
-	c.generateList(length)
-	c.printList()
-#	myList = []
-#	for i in range(length):
-#		myList.append(randint(0,100))
-#	c.printList(myList)
-#	print("-----------------------------")
-#	for i in range(length):
-#		print(myList[i], end=' ')
-#	print("\n-----------------------------\n")
-	
+	c.generateList(length, size)
+	c.printList()	
 	while not choiceDone:
 		print("Which sort method would you like to use?")
-		instring = input("(1)BubbleSort (2)CoctailShakerSort (3)Quit\n> ")
+		instring = input("(1)Bubble Sort (2)Insertion Sort (3)Quit\n> ")
 		if instring is "1":
-			print("You chose BubbleSort")
+			print("You chose Bubble Sort")
 			c.bubbleSort(length)
 			c.printList()
+			c.freq()
 			choiceDone = True
-		if instring is "2":
-			print("you chose CoctailShakerSort")
+		elif instring is "2":
+			print("you chose Insertion Sort")
+			c.insertionSort()
+			c.printList()
+			c.freq()
 			choiceDone = True
-		if instring is "3":
+		elif instring is "3":
 			choiceDone = True
 			done = True
-#		else 
-#			print("type either 1 or 2")
+		else: 
+			print("Please type a number between 1 and 3.")
 
